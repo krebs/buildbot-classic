da5c0204e89ba5c2f431a712b9ca1c32e772fa5e
makefu <github@syntax-fehler.de>
2017-08-04 22:15:51 +0200

    master: unpin sqlalchemy

--- master/setup.py
+++ master/setup.py
@@ -388,11 +388,10 @@ else:
         ]
 
     setup_args['install_requires'] += [
-        # sqlalchemy-0.8 betas show issues with sqlalchemy-0.7.2, so stick to 0.7.10
-        'sqlalchemy >= 0.6, <= 0.7.10',
+        'sqlalchemy >= 0.6',
         # buildbot depends on sqlalchemy internals, and this is the tested
         # version.
-        'sqlalchemy-migrate==0.7.2',
+        'sqlalchemy-migrate>=0.7.2',
         'python-dateutil>=1.5',
     ]
     setup_args['tests_require'] = [
